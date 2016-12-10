1060d29b5764a0ef57871778c6c1787496373d7f
VaRudenko <bajijiu@gmail.com>
2016-12-10 13:35:02 +0200

    Home1

---
+++ src/com/hillel/homework/Homework1.java
@@ -0,0 +1,13 @@
+package com.hillel.homework;
+
+public class Homework1 {
+	public static void main(String[] args) {
+		
+	int v = 30;
+	int t = 25;
+	
+	int c = v * t;
+	
+	System.out.println(c + "  The number of kilometers");
+	}
+}
